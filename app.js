3430c3319a735e32fa7ad294ed4ec0cc744e294b
SalomonFresh23 <fuadsalomon23@gmail.com>
2025-08-17 20:23:45 -0400

    Funcion AgregarAmigp

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreInput = document.getElementById("amigo").value;
+    amigos.push(nombreInput);
+    console.log(amigos)
+}
