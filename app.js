748e9c9921ec4cfa72947bcbd3deae1f96e0aed1
SalomonFresh23 <fuadsalomon23@gmail.com>
2025-08-20 00:28:40 -0400

    Agregar Funcion listar amigos

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
+//variables globales
 let amigos = [];
+let inputController = document.querySelector("#amigo");
+let listaAmigos = document.querySelector("#listaAmigos")
+
 
 function agregarAmigo() {
+
+
+    if(inputController.value == ""){
+        alert("Debes ingresar un nombre valido")
+
+    }else {
     let nombreInput = document.getElementById("amigo").value;
     amigos.push(nombreInput);
     console.log(amigos)
+    
+    inputController.value = "";
+    listarAmigos();
+    }
 }
+
+
+function listarAmigos(){
+    listaAmigos.innerHTML = "";
+
+        for (let index = 0; index < amigos.length; index++) {
+          let nombre = document.createElement("li");
+          nombre.textContent = amigos[index];
+          listaAmigos.appendChild(nombre)
+        }
+    }
+
